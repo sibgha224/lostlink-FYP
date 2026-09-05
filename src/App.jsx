776ce1d350b43e00eb0f37189aca9aa22d5e7bf9
Alishba9437 <mesham600@gmail.com>
2026-09-05 09:27:32 -0700

    Update App.jsx routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLogin from "./pages/adminlogin";
 import AdminDashboard from "./pages/admin-dashboard";
 import LostItems from "./pages/lostItems";
+import Claims from "./pages/claims-page"; // Claims page import kiya
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/lost-items" element={<LostItems />} />
+        <Route path="/admin/claims" element={<Claims />} /> {/* Claims route add ho gaya */}
       </Routes>
     </Router>
   );
